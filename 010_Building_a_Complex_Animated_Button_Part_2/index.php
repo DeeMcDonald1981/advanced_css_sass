1d3ba01cba0cb558936e413604ba5f6b6ee6325b
virgoEnigma1981 <dee.mcdonald33@gmail.com>
2019-05-13 00:26:55 -0500

    created the header file animiated the hero text and animated the hero button

---
+++ 010_Building_a_Complex_Animated_Button_Part_2/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Natours | Exciting tours for adventerous people</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<header class="header">
+	 	<div class="logo_box">
+             <img src="./img/logo-white.png" alt="Logo" class="logo">
+         </div>
+        <div class="text_box">
+            <h1 class="heading_primary">
+                <span class="heading_primary_main">OutDoors</span>
+                <span class="heading_primary_sub">is where life happens</span>
+						</h1>
+						<a href="#" class="btn btn_white btn_animated">Discover our tours</a>
+        </div><!-- text_box -->
+	</header>
+</body>
+</html>
